3fb39162e13019797978595c36e8eb08cff0bccc
Peter Nilsson <peter8nilsson@live.se>
2024-01-16 22:11:17 +0100

    Added list command which lists all commits in the current project

---
+++ gitf/Source/Commands/ListCommitsCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace gitf.Commands;
+
+internal class ListCommitsCommand : Command
+{
+	public override string GetName()
+	{
+		return "list";
+	}
+
+	public override string GetDescription()
+	{
+		return "Lists all commits in this project.";
+	}
+
+	public override string GetUsage()
+	{
+		return "gitf list";
+	}
+
+	public override bool Execute(CommandData data, ReadOnlySpan<string> args, ref List<string> messages)
+	{
+		foreach (var commit in data.Project.Commits)
+		{
+			messages.Add($"{commit.Name} ({commit.Files.Count} files)");
+		}
+
+		return true;
+	}
+}
